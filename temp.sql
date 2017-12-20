0ef20d9a4f8e3d496ae571eb4a0a00ae363302a5
Vielmas Florent <florent.vielmas@etu.umontpellier.fr>
2017-12-20 15:07:34 +0100

    refactoring

--- temp.sql
+++
@@ -1,6 +0,0 @@
-http://localhost/~fvielmas/CarShare/traitementformulaire.php?nom=Cordoba&prenom=Alicia&email=corrielalicia%40gmail.com&password=koko&pseudo=ali&date_naissance=2017-12-04&adr=5+rue+des+lilas&tel=0147895623
-
-
-/*
-*/
-
