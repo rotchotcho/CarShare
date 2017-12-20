7671cd74bef1152b765643563df71f44b79bdfd5
cbourdeau <bourdeauclement@hotmail.fr>
2017-12-20 15:43:53 +0100

    Modif acceuil

---
+++ inscription.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>	
+<html>
+  <head>
+   <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+   <script src="bootstrap/js/jquery.js"></script>
+    <meta charset="utf-8"/>
+    <title>Car Share, le site de covoiturage</title>
+  </head>
+  <body>
+    <h1>Bienvenue sur Car share, le site numéro du covoiturage</h1>
+     <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+	<a href="accueil.html">Accueil</a>
+	<p>Inscription :</p>
+	<form method="get" action="traitementformulaire.php">
+	  Nom* : <input name="nom" type="text" required><br/>
+	  Prénom* : <input name="prenom" type="text" required><br/>
+	  Email* : <input name="email" type="email" required><br/>
+	  Mot de passe* : <input name="password" type="password" required><br/>
+	  Pseudonyme : <input name="pseudo" type="text"><br/>
+	  Date de naissance* : <input name="date_naissance" type="date" required><br/>
+	  Adresse* : <input name="adr" type="text" required><br/>
+	  Tel* : <input name="tel" type="text" pattern="[0-9]{10}" required><br/>
+	  <input type="submit" value="Je m'inscris !">
+	</form>
+	<p>* Champs obligatoires</p>
+  </body>
+ 
+</html>
