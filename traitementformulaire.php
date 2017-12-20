2180fef165417ec347bcb5af8dcf5e5906609a6a
Vielmas Florent <florent.vielmas@etu.umontpellier.fr>
2017-12-20 13:50:32 +0100

    enrgistrement par formulaire ok

--- traitementformulaire.php
+++ traitementformulaire.php
@@ -9,42 +9,49 @@ $pseudo = $_GET['pseudo'];
 $date_nai = $_GET['date_naissance'];
 $adr = $_GET['adr'];
 $tel = $_GET['tel'];
-echo 'Bonjour, '.$prenom.' '.$nom;
-echo '-- ' .$email.' '.$mdp;
-echo '-- '.$pseudo.' '.$date_nai;
-echo '-- ' .$adr.' '.$tel; 
-
+echo 'Enregistrement dans la base de: <br/>--'.$prenom.' '.$nom;
+echo '<br/>-- ' .$email.' '.$mdp;
+echo '<br/>-- '.$pseudo.' '.$date_nai;
+echo '<br/>-- ' .$adr.' '.$tel; 
 
        $dsn = "mysql:host=prodpeda-venus; dbname=fvielmas";
        $user = "fvielmas";
        $pass = "garrinch";
        
        $pdo = new PDO($dsn, $user, $pass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+echo '<br/><br/>--> Connexion à la base de donnée OK'; 
+
+
 
 
 ////inscription du nouveau membre  dans la base donnée
 
-$insert = $pdo->prepare("INSERT INTO MEMBRE (id, ptype,nom, prenom) VALUES (41,'UTILISATEUR',:nom,:prenom)");
-//$insert->bindParam(':email', $email);
-//$insert->bindParam(':mdp', $mdp);
+$insert = $pdo->prepare("INSERT INTO MEMBRE (id,ptype,email,mdp,nom,prenom,date_naissance,adresse,tel) VALUES (41,'UTILISATEUR',:email,:mdp,:nom,:prenom,:date_naissance,:adresse,:tel)");
+
+
+$insert->bindParam(':email', $email);
+$insert->bindParam(':mdp', $mdp);
 $insert->bindParam(':nom', $nom);
+$insert->bindParam(':date_naissance', $date_nai);
 $insert->bindParam(':prenom', $prenom);
-//$insert->bindParam(':adr', $adr);
-//$insert->bindParam(':tel', $tel);
+$insert->bindParam(':adresse', $adr);
+$insert->bindParam(':tel', $tel);
 $insert->execute();
 
+echo '<br/>--> Insertion du nouveau membre dans la base de donnée ok';
 
-$sql = 'SELECT nom, prenom FROM MEMBRE'; 
+$sql = 'SELECT id, nom, prenom, date_naissance, adresse, tel, email, mdp FROM MEMBRE'; 
     $query = $pdo->query($sql);
     $tuples = $query->fetchAll(PDO::FETCH_OBJ);
     
     $nom = $tuple->nom;
     echo "<ul>";
       foreach($tuples as $tuple) {
-      echo "<li>",$tuple->nom," ",$tuple->prenom,"</li>";
+      echo "<li>",$tuple->id," ",$tuple->nom," ",$tuple->prenom," ",$tuple->date_naissance,' ',$tuple->adresse," ",$tuple->tel," ",$tuple->email," ",$tuple->mdp,"</li>";
       }
     echo"</ul>";
-
+echo '<br/>--> affichage des membres actualisés';
     ?>
+
 
 <p>retour sur l'inscription : <a href="inscription.php"> clique ici</a> pour revenir à la page inscription.</p>    
